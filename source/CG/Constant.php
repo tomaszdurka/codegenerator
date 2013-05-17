2442f8013d4b21a07b898e2b7962420759752c56
Tomasz Durka <tomasz@durka.pl>
2013-05-17 19:51:21 +0200

    Add Constant class

---
+++ source/CG/Constant.php
@@ -0,0 +1,30 @@
+<?php
+
+class CG_Constant extends CG_Block {
+
+	/** @var string */
+	private $_name;
+
+	/** @var string|int */
+	private $_value;
+
+	/**
+	 * @param string $name
+	 * @param string|int $value
+	 */
+	public function __construct($name, $value) {
+		$this->_name = (string) $name;
+		$this->_value = $value;
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getName() {
+		return $this->_name;
+	}
+
+	public function dump() {
+		return 'const ' . $this->_name . ' = ' . var_export($this->_value, true) . ';';
+	}
+}
