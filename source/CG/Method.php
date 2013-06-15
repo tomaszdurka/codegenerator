c3037d292ebb293e7ac21c5935719fc5cb2eb22f
Tomasz Durka <tomasz@durka.pl>
2013-06-15 02:58:58 +0200

    Add additional extract data from ReflectionMethod (static, visibility)

--- source/CG/Method.php
+++ source/CG/Method.php
@@ -25,8 +25,22 @@ class CG_Method extends CG_Function {
 		$this->_visibility = (string) $visibility;
 	}
 
+	/**
+	 * @param boolean $static
+	 */
 	public function setStatic($static) {
 		$this->_static = (bool) $static;
+	}
+
+	/**
+	 * @param ReflectionFunctionAbstract $reflection
+	 */
+	public function extractFromReflection(ReflectionFunctionAbstract $reflection) {
+		parent::extractFromReflection($reflection);
+		if ($reflection instanceof ReflectionMethod) {
+			$this->_setVisibilityFromReflection($reflection);
+			$this->_setStaticFromReflection($reflection);
+		}
 	}
 
 	protected function _dumpHeader() {
@@ -36,6 +50,28 @@ class CG_Method extends CG_Function {
 		}
 		$code .= ' ' . parent::_dumpHeader();
 		return $code;
+	}
+
+	/**
+	 * @param ReflectionMethod $reflection
+	 */
+	private function _setVisibilityFromReflection(ReflectionMethod $reflection) {
+		if ($reflection->isPublic()) {
+			$this->setVisibility('public');
+		}
+		if ($reflection->isProtected()) {
+			$this->setVisibility('protected');
+		}
+		if ($reflection->isPrivate()) {
+			$this->setVisibility('private');
+		}
+	}
+
+	/**
+	 * @param ReflectionMethod $reflection
+	 */
+	private function _setStaticFromReflection(ReflectionMethod $reflection) {
+		$this->setStatic($reflection->isStatic());
 	}
 
 	/**
